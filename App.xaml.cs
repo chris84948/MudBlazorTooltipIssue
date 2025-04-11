ac1e566cb911585f833210c4aec1bb5e565ca04a
chris84948 <chris84948@gmail.com>
2025-04-11 12:23:18 -0500

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using System.Windows;
+
+namespace WpfBlazorApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
